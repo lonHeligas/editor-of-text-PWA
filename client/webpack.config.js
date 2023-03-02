5add297bb90bbea48574a4950802ff8b0aabeb1e
Lon Heligas <lon.heligas@gmail.com>
2023-03-01 20:43:56 -0600

    debugging some more but not getting anywhere

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     
     plugins: [ 
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: './index.html',        
         title: 'J.A.T.E.' 
       }),
       new InjectManifest({
